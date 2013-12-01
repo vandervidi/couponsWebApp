afa15cf0a9a060b81286fbec8998917b186e4c34
ofir agahi <iofirag@gmail.com>
2013-12-01 15:02:55 +0200

    fix pass in MD5 hash format

--- src/com/oa/couponsWebApp/User.java
+++ src/com/oa/couponsWebApp/User.java
@@ -1,5 +1,9 @@
 package com.oa.couponsWebApp;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
 import javax.persistence.*;
@@ -16,7 +20,7 @@ public class User {
 	public User(String userName, String password, int privilige) {
 		super();
 		this.userName = userName;
-		this.password = password;
+		this.setPassword(password);
 		this.privilige=privilige; //1=Admin, 0=Regular user
 		this.dateOfRegistration = new Date();
 	}
@@ -24,11 +28,10 @@ public class User {
 	public User(String userName, String password) {
 		super();
 		this.userName = userName;
-		this.password = password;
+		this.setPassword(password);
 		this.privilige=0;  //1=Admin, 0=Regular user
 		this.dateOfRegistration = new Date();
 	}
-
 
 	public User() {
 		// TODO Auto-generated constructor stub
@@ -42,26 +45,39 @@ public class User {
 	public void setUsername(String userName) {
 		this.userName = userName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
-		this.password = password;
-	}
-	public Date getDateOfRegistration() {
-		return dateOfRegistration;
-	}
-	public void setDateOfRegistration(Date dateOfRegistration) {
-		this.dateOfRegistration = dateOfRegistration;
+		String encryptedPass = MD5(password);
+		if (encryptedPass!= null){
+			this.password = encryptedPass;
+		}
 	}
 	
-	
-	@Override
-	public String toString() {
-		return "Coupon [userName=" + userName + ", password=" + password + ", dateOfRegistration="
-				+ dateOfRegistration + "]";
+	// Using MD5
+	public String MD5(String input) {
+		String md5 = null;
+		
+        if( input == null) return null;
+        
+        try {
+	        //Create MessageDigest object for MD5
+	        MessageDigest digest = MessageDigest.getInstance("MD5");
+	         
+	        //Update input string in message digest
+	        digest.update(input.getBytes(), 0, input.length());
+	 
+	        //Converts message digest value in base 16 (hex) 
+	        md5 = new BigInteger(1, digest.digest()).toString(16);
+ 
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return md5;
 	}
-
+	
 	public int getPrivilige() {
 		return privilige;
 	}
@@ -69,4 +85,22 @@ public class User {
 	public void setPrivilige(int privilige) {
 		this.privilige = privilige;
 	}
+	
+	public Date getDateOfRegistration() {
+		return dateOfRegistration;
+	}
+	
+	public void setDateOfRegistration(Date dateOfRegistration) {
+		this.dateOfRegistration = dateOfRegistration;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Coupon [userName=" + userName + ", password=" + password + ", dateOfRegistration="
+				+ dateOfRegistration + ", privilige=" + privilige + "]";
+	}
+
+	
 }
